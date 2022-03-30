bd77779c163eade582b80cd02f6c8c57f954a1e1
Przemyslaw Mejna <pmejna@gmail.com>
2022-03-30 23:17:20 +0200

    remove the client interface and refactor company interface. Create company module controller and service

--- src/interfaces/iCompany.ts
+++ src/interfaces/iCompany.ts
@@ -1,13 +1,21 @@
+export type companyType = 'company_designer' | 'company_brand';
 export interface CompanyInterface {
   company_uuid: string;
+  company_type: companyType;
   company_address_line1: string;
-  company_address_line2: string;
+  company_address_line2?: string;
+  company_address_country: string;
+  company_address_local_region?: string;
+  company_address_town: string;
+  company_address_postcode: string;
   company_phone1: string;
-  company_phone2: string;
-  company_phone3: string;
+  company_phone2?: string;
+  company_phone3?: string;
   company_email: string;
-  company_description: string;
-  company_register_number: string;
+  company_email2?: string;
+  company_email3?: string;
+  company_description?: string;
+  company_register_number?: string;
   company_created_datetime: string;
   company_updated_datetime: string;
 }
